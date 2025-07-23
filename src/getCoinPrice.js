448acaa4534fde883f086ad9bf8c27d8dd9c04c0
Pedro Paulo <pedropaulocg@MacBook-Pro-de-Pedro.local>
2025-07-23 19:48:13 -0300

    new api

--- src/getCoinPrice.js
+++ src/getCoinPrice.js
@@ -3,9 +3,9 @@ import axios from "axios"
 export const getCoinPrice = async (coin) => {
   try {
     coin = coin.toUpperCase()
-    const response = await axios.get(`https://api.frankfurter.app/latest?from=${coin}&to=BRL`)
-    const data = response.data
-    const price = data.rates.BRL
+    const response = await axios.get(`https://economia.awesomeapi.com.br/json/last/${coin}-BRL?token=${process.env.API_KEY}`)
+    const data = response.data[`${coin}BRL`]
+    const price = data.ask
     return {text: `O preço da moeda ${coin} é: R$ ${parseFloat(price).toFixed(2)}`}
   } catch (error) {
     console.log(error)
